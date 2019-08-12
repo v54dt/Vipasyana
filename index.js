786993ddca131ed198acbbeff3194687e20c2505
v54dt <vincent71497867@gmail.com>
2019-08-12 20:08:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ app.get('/api/getuser',(req,res)=>{
 app.listen(3000,()=>{
     console.log('Listen to port 3000...');
 });
+
+
+//Set Header Check (async)
+app.use(function (req,res,next){
+    var api_key = req.key('API-KEY');
+
+    if(api_key !=    "test"    ){
+        res.status(401).send({error: "Unauthorized"});
+    }
+    else{
+        next();
+    }
+});
