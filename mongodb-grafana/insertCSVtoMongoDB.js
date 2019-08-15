2b99e1d6b5168c8fe9821b2c935008980e97d20f
v54dt <vincent71497867@gmail.com>
2019-08-15 14:03:03 +0800

    Update insertCSVtoMongoDB.js

--- mongodb-grafana/insertCSVtoMongoDB.js
+++ mongodb-grafana/insertCSVtoMongoDB.js
@@ -1,38 +1,37 @@
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
+var url = "mongodb://";
 
-MongoClient.connect(url, function(err, db)
- {
+MongoClient.connect(url, function (err, db) {
   if (err) throw err;
   var db_read = db.db("Test0727");
   var db_write = db.db("Test0727");
-  db_read.collection("0716").findOne({}, function(err, result) 
-  {
-    var sensor_mac ='00000001-0000-0000-0000-D2AC31BF0A1C';
+  db_read.collection("0716").findOne({}, function (err, result) {
+    if (err) throw err;
+    var sensor_mac = '00000001-0000-0000-0000-D2AC31BF0A1C';
     var semsor_rssi = -63;
-    
-    
+
+
     const csv = require('csv-parser');
     const fs = require('fs');
 
-    fs.createReadStream('ecg_data_0.csv')
-    .pipe(csv())
-    .on('data', (row) => 
-    {
-        if(row.device_id ==3)
-        {
-            var t = Number(Math.floor(row.timestamp));
-            var mongo_obj = { MAC : sensor_mac,ecg_data : row.data, ts : new Date(t)};
-            db_write.collection("0322_fake_ecg_data").insertOne(mongo_obj);
+    fs.createReadStream('C:/Users/黃柏維/Desktop/nodejs/ecg_data_0.csv')
+      .pipe(csv())
+      .on('data', (row) => {
+        if (row.device_id == 3) {
+          var current_time = Number(Date.now());
+          var start_time = 1553240835494;
+          var t_shift = current_time - start_time;
+          var t = Number(Math.floor(row.timestamp)) + t_shift;
+          var mongo_obj = { MAC: sensor_mac, device_id: row.device_id, ecg_data: Number(row.data), ts: t };
+          db_write.collection("0814_csv").insertOne(mongo_obj);
         }
-    })
-    .on('end', () => 
-    {
+      })
+      .on('end', () => {
         console.log('CSV file successfully processed');
-    });
+      });
 
-   
+
   });
   //db.close(); asyc need to fix
 
